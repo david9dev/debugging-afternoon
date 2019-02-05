775cbb5899777a62e3dfb3384988f333c42aab55
David Wells <davidwells@Davids-MacBook-Pro.local>
2019-02-05 16:00:11 -0700

    finished stage 4

--- stage-4/src/index.js
+++ stage-4/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {HashRouter as Router} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -9,7 +10,9 @@ import store from './redux/store';
 ReactDOM.render(
 
     <Provider store={store}>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
